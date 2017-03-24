b844fd75b123eca4b4a43ed3ecbca80f27f796f7
Jonathan Eidelman <jonathaneidelman@Jonathans-MacBook-Air.local>
2017-03-23 20:36:03 -0400

    Fixed length overflow error and added additional tests

--- KnowRoaming/generate_db.sql
+++ KnowRoaming/generate_db.sql
@@ -7,12 +7,10 @@ DROP TABLE IF EXISTS user_records;
 
 
 CREATE TABLE user_records (
---       ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	   
        unique_id VARCHAR(20) NOT NULL PRIMARY KEY,
        name VARCHAR(20),
        email VARCHAR(50) UNIQUE,
-       phone_number VARCHAR(10));
+       phone_number VARCHAR(20));
 
        
 CREATE TABLE data_types (
@@ -25,8 +23,6 @@ CREATE TABLE usage_records (
        ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
        user_ID VARCHAR(20) NOT NULL,
        tp_ID int NOT NULL,
- --      start_date date,
- --      end_date date,
  	   time_stamp date,
        FOREIGN KEY (user_ID) REFERENCES user_records(unique_id),
        FOREIGN KEY (tp_ID) REFERENCES data_types(ID)
